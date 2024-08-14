3dbb1da15fd644efe9e248c08476f5703bd79a39
student-gettanjali <geetanjalirawat691@gmail.com>
2024-08-14 17:07:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick= () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+    console.log("button clicked")
+}
+let searchForm = document.querySelector(".search-form");
+document.querySelector('#search-btn').onclick= () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+    console.log("button clicked")
+}
+let cartItem = document.querySelector(".cart-items-container");
+document.querySelector('#cart-btn').onclick= () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    console.log("button clicked")
+}
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
